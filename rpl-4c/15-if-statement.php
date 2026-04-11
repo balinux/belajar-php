aabc0814d5c68721cb55e5ef8aff65f8790f4c60
balinux <ryojuniyantara@gmail.com>
2026-04-11 15:36:00 +0800

    upload: untuk kelas 4C

---
+++ rpl-4c/15-if-statement.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = "echa";
+$grade = 70;
+$standar = 75;
+$remidial = 60;
+
+if($grade > $standar) {
+    echo "Lulus";
+}elseif($remidial > $standar) {
+    echo "Lulus jalur Remedial";
+}else{
+    echo "Tidak Lulus";
+}
+
+// tugas 
+// tambahkan logikan perbandingan
---
+++ rpl-4c/9-operator-aritmatika.php
@@ -0,0 +1,21 @@
+<?php
+
+$var1 = 10; 
+$var2 = 20;
+
+// penjumlahan
+$hasil = $var1 + $var2;
+echo $hasil;
+echo "\n";
+
+// pengurangan
+$hasil2 = $var1 - $var2;
+echo $hasil2;
+echo "\n";
+
+// tugas
+// perkalian
+// pembagian
+// modulus
+
+
