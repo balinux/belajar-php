a174b93c2efec4001142b9f0e8dc136b6482bdff
balinux <ryojuniyantara@gmail.com>
2026-05-02 15:05:04 +0800

    add: menggunakan __construct untuk mengubah nilai property

--- rpl-4c/30-class.php
+++ rpl-4c/30-class.php
@@ -5,27 +5,32 @@ class Person{
   public string $name;  
   public string $address;
 
+  public int $ageProperty;
+
   const VERSI_APLIKASI = "1.0.0";
 
   // method
   public function sayHello(string $name){
     // halo nama kamu adalah: fulan
     echo "halo nama kamu adalah: $name dan alamat di: $this->address".PHP_EOL;
+    echo "usia kamu adalah: $this->ageProperty tahun".PHP_EOL;
     echo "versi aplikasi: " . self::VERSI_APLIKASI . PHP_EOL;
   }
 
 //   function construct adalah sebuah function yang di panggil pertama kali saat object di buat
 
-public function __construct(){
-    echo "ini adalah function yang pertama kali di panggil".PHP_EOL;
+public function __construct(int $ageParameter){
+    // mengubah nilai property age dari contruct
+    $this->ageProperty = $ageParameter;
 }
  
 }
 
 // intance sebuah class ke dalam object
 // menjalankan fungsi construct
-$mahasiswa = new Person();
+$mahasiswa = new Person(20);
 // ubah property
 $mahasiswa->address = "selong";
+
 // memanggil function
 $mahasiswa->sayHello("kamal");
